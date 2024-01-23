0243a160ae8f8d907b2a413180c106a7368069a7
Alexander Slesarenko <avslesarenko@gmail.com>
2024-01-23 22:11:51 +0100

    removed ergo-p2p

--- ergo-p2p/src/constants.rs
+++
@@ -1,4 +0,0 @@
-use std::time::Duration;
-
-/// The timeout for handshakes when connecting to new peers.
-pub const HANDSHAKE_TIMEOUT: Duration = Duration::from_secs(4);
--- ergo-p2p/src/message/request.rs
+++
@@ -1,3 +0,0 @@
-/// A network request
-#[derive(Clone, Debug, Eq, PartialEq)]
-pub enum Request {}
