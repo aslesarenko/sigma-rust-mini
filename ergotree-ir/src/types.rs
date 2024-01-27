3ad712d9ac6d3a685568a58d63fc4dcf879803fa
Alexander Slesarenko <avslesarenko@gmail.com>
2024-01-27 21:13:22 +0100

    sigma-mini-rs: removed SMethod descriptors

--- ergotree-ir/src/types.rs
+++ ergotree-ir/src/types.rs
@@ -2,18 +2,12 @@
 
 pub mod stype;
 
-/// Collection object type companion
-pub mod scoll;
 /// Function signature type
 pub mod sfunc;
-/// Global methods
-pub mod sglobal;
 /// GroupElement methods
-pub mod sgroup_elem;
 /// Object method(property) signature type
 pub mod smethod;
 /// Option object type companion
-pub mod soption;
 /// Tuple type
 pub mod stuple;
 /// Type companion for an object
--- ergotree-ir/src/types/stype_companion.rs
+++ ergotree-ir/src/types/stype_companion.rs
@@ -4,10 +4,6 @@ use std::fmt::Debug;
 use crate::serialization::types::TypeCode;
 use crate::serialization::SigmaParsingError;
 
-use super::scoll;
-use super::sglobal;
-use super::sgroup_elem;
-use super::soption;
 use strum::IntoEnumIterator;
 use strum_macros::EnumIter;
 
@@ -24,24 +20,36 @@ pub enum STypeCompanion {
     Option,
 }
 
+/// SGlobal type name
+pub static GLOBAL_TYPE_NAME: &str = "Global";
+/// SColl type name
+pub static COLL_TYPE_NAME: &str = "Coll";
+
+/// SGroupElement type name
+pub static GROUP_ELEMENT_TYPE_NAME: &str = "GroupElement";
+
+/// SOption type name
+pub static OPTION_TYPE_NAME: &str = "Option";
+
 impl STypeCompanion {
+
     /// Get object's type code
     pub fn type_code(&self) -> TypeCode {
         match self {
-            STypeCompanion::Coll => scoll::TYPE_CODE,
-            STypeCompanion::GroupElem => sgroup_elem::TYPE_CODE,
-            STypeCompanion::Global => sglobal::TYPE_CODE,
-            STypeCompanion::Option => soption::TYPE_CODE,
+            STypeCompanion::Coll => TypeCode::COLL,
+            STypeCompanion::GroupElem => TypeCode::SGROUP_ELEMENT,
+            STypeCompanion::Global => TypeCode::SGLOBAL,
+            STypeCompanion::Option => TypeCode::OPTION,
         }
     }
 
     /// Get object's type name
     pub fn type_name(&self) -> &'static str {
         match self {
-            STypeCompanion::Coll => scoll::TYPE_NAME,
-            STypeCompanion::GroupElem => sgroup_elem::TYPE_NAME,
-            STypeCompanion::Global => sglobal::TYPE_NAME,
-            STypeCompanion::Option => soption::TYPE_NAME,
+            STypeCompanion::Coll => COLL_TYPE_NAME,
+            STypeCompanion::GroupElem => GROUP_ELEMENT_TYPE_NAME,
+            STypeCompanion::Global => GLOBAL_TYPE_NAME,
+            STypeCompanion::Option => OPTION_TYPE_NAME,
         }
     }
 }
